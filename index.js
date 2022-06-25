d9778921fdfe9e05d13c7e52328442bc2039359c
Pop Dorian <dorianpop1512@gmail.com>
2022-06-25 21:17:42 +0300

    Added the possibility of deleting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const persons = [
+let persons = [
     { 
         "id": 1,
         "name": "Arto Hellas", 
@@ -37,6 +37,20 @@ app.get('/info', (request, response) =>{
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>`);
 
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+    if(!person)
+        return response.status(404).end();
+    response.json(person);
+})
+
+app.delete('/api/persons/:id', (request, response) =>{
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+    response.status(204).end();
 })
 
 const PORT = 3001;
